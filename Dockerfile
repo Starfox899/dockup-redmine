10bf7333f96aad4d1de74ec53714e3c00c7542f4
Sascha Herrmann <sascha.herrmann@web.de>
2015-12-01 10:07:42 +0100

    Added functionality to backup postgres, and customized it for backing up redmine (e.g. sameersbn/redmine stack)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM ubuntu:trusty
-MAINTAINER Borja Burgos <borja@tutum.co>, Mia Iversen <mia@chillfox.com
+FROM ubuntu:15.04
+MAINTAINER Sascha Herrmann
 
-RUN apt-get update && apt-get install -y python-pip && pip install awscli
+RUN apt-get update 
+RUN apt-get install -y python-pip && pip install awscli
+RUN apt-get install -y postgresql-client
+RUN apt-get clean -y
 
 ADD backup.sh /backup.sh
 ADD restore.sh /restore.sh
@@ -11,9 +14,15 @@ RUN chmod 755 /*.sh
 ENV S3_BUCKET_NAME docker-backups.example.com
 ENV AWS_ACCESS_KEY_ID **DefineMe**
 ENV AWS_SECRET_ACCESS_KEY **DefineMe**
-ENV AWS_DEFAULT_REGION us-east-1
-ENV PATHS_TO_BACKUP /paths/to/backup
-ENV BACKUP_NAME backup
+ENV AWS_DEFAULT_REGION eu-central-1
+# FIXME REDMINE_FILES should be taken from link to redmine container
+ENV REDMINE_FILES /home/redmine/data
+# ENV POSTGRES_ENV_DB_NAME **ToBeDefinedByLinkingToPostgresContainer**
+# ENV POSTGRES_ENV_DB_USER **ToBeDefinedByLinkingToPostgresContainer**
+# ENV POSTGRES_ENV_DB_PASS **ToBeDefinedByLinkingToPostgresContainer**
+# ENV POSTGRES_PORT_5432_TCP_ADDR **ToBeDefinedByLinkingToPostgresContainer**
+# ENV POSTGRES_PORT_5432_TCP_PORT **ToBeDefinedByLinkingToPostgresContainer**
+ENV BACKUP_NAME backup-redmine
 ENV RESTORE false
 
 CMD ["/run.sh"]
--- backup.sh
+++ backup.sh
@@ -1,18 +1,71 @@
 #!/bin/bash
 
+# Exit script as soon as first error is observed (see http://stackoverflow.com/a/1379904/2161065)
+set -e
+
+#
+# Sanity checks for necessary environment vars
+#
+echo "--- Checking for necessary environment variables ---"
+: "${BACKUP_NAME:?BACKUP_NAME needs to be defined!}"
+: "${REDMINE_FILES:?REDMINE_FILES needs to be defined!}"
+: "${S3_BUCKET_NAME:?S3_BUCKET_NAME needs to be defined!}"
+: "${POSTGRES_PORT_5432_TCP_ADDR:?POSTGRES_PORT_5432_TCP_ADDR needs to be defined!}"
+: "${POSTGRES_PORT_5432_TCP_PORT:?POSTGRES_PORT_5432_TCP_PORT needs to be defined!}"
+: "${POSTGRES_ENV_DB_NAME:?POSTGRES_ENV_DB_NAME needs to be defined!}"
+: "${POSTGRES_ENV_DB_USER:?POSTGRES_ENV_DB_USER needs to be defined!}"
+: "${POSTGRES_ENV_DB_PASS:?POSTGRES_ENV_DB_PASS needs to be defined!}"
+
+
 # Get timestamp
-: ${BACKUP_SUFFIX:=.$(date +"%Y-%m-%d-%H-%M-%S")}
-readonly tarball=$BACKUP_NAME$BACKUP_SUFFIX.tar.gz
+echo "--- Generating timestamped filenames ---"
+: ${BACKUP_SUFFIX:=$(date +"%Y-%m-%d-%H-%M-%S")}
+readonly tarball=$BACKUP_NAME-files-$BACKUP_SUFFIX.tar.gz
+readonly pgdump=$BACKUP_NAME-postgres-$BACKUP_SUFFIX.sqlc
+
+#
+# Redmine files backup
+#
+echo "--- Creating backup of redmine files ---"
 
 # Create a gzip compressed tarball with the volume(s)
-tar czf $tarball $BACKUP_TAR_OPTION $PATHS_TO_BACKUP
+tar czf $tarball $BACKUP_TAR_OPTION $REDMINE_FILES
+
+#
+# Redmine postgres backup
+#
+
+echo "--- Creating backup of postgres database ---"
+# Create .pgpass file in home directory, and set it to 600 (see http://stackoverflow.com/questions/2893954/how-to-pass-in-password-to-pg-dump)
+echo "$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT:${POSTGRES_ENV_DB_NAME}:${POSTGRES_ENV_DB_USER}:${POSTGRES_ENV_DB_PASS}" > ~/.pgpass
+chmod 600 ~/.pgpass
+
+# Create backup of database
+/usr/bin/pg_dump \
+  -h $POSTGRES_PORT_5432_TCP_ADDR \
+  -p $POSTGRES_PORT_5432_TCP_PORT \
+  -d $POSTGRES_ENV_DB_NAME \
+  -U $POSTGRES_ENV_DB_USER \
+  -Fc \
+  --file=$pgdump
+rm -rf ~/.pgpass
+
+#
+# Upload backup files to AWS S3
+#
+echo "--- Uploading backups to AWS S3 ---"
 
 # Create bucket, if it doesn't already exist
 BUCKET_EXIST=$(aws s3 ls | grep $S3_BUCKET_NAME | wc -l)
-if [ $BUCKET_EXIST -eq 0 ]; 
+if [ $BUCKET_EXIST -eq 0 ];
 then
   aws s3 mb s3://$S3_BUCKET_NAME
 fi
 
-# Upload the backup to S3 with timestamp
+# Upload the files backup to S3 with timestamp
 aws s3 cp $tarball s3://$S3_BUCKET_NAME/$tarball
+
+# Upload the psql backup to S3 with timestamp
+aws s3 cp $pgdump s3://$S3_BUCKET_NAME/$pgdump
+
+echo "--- Backup finished! ---"
